6b87610558d410d7fee4a70bb298f0e75857a10a
Edison Achote <erachote@utpl.edu.ec>
2023-12-04 15:39:28 -0500

    ejemplopython

--- EjemploPython/Ejemplo1.py
+++ EjemploPython/Ejemplo1.py
@@ -1,1 +1,25 @@
-print("Hola mundo")
+class Vehiculo:
+    def __init__(self, modelo, color, año):
+        self.modelo = modelo
+        self.color = color
+        self.año = año
+
+    def obtener_informacion(self):
+        return f"Vehículo: {self.modelo}, Color: {self.color}, Año: {self.año}"
+
+
+class Empresa:
+    def __init__(self, nombre, direccion):
+        self.nombre = nombre
+        self.direccion = direccion
+
+    def obtener_informacion(self):
+        return f"Empresa: {self.nombre}, Dirección: {self.direccion}"
+
+
+# Ejemplo de uso:
+mi_vehiculo = Vehiculo("Toyota hilux", "Azul", 2022)
+mi_empresa = Empresa("ABC Motors", "123 Calle Principal")
+
+print(mi_vehiculo.obtener_informacion())
+print(mi_empresa.obtener_informacion())
